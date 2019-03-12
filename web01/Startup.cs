b8debd03312a8e640fa1c3b56fbb51f8e37c390d
fl0ekill <highwizard_14@hotmail.com>
2019-03-12 23:36:41 +0700

    startup

--- web01/Startup.cs
+++ web01/Startup.cs
@@ -48,6 +48,7 @@ namespace web01 {
                 c.SwaggerEndpoint ("/swagger/v1/swagger.json", "My API V1");
             });
 
+
             // app.UseHttpsRedirection ();
             app.UseMvc ();
         }
